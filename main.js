bc8b1c1c9a87c8e011df1a5329d26b127bf62644
ravitejadiggum <142608086+ravitejadiggum@users.noreply.github.com>
2024-06-20 21:37:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+function submitinput(){
+    document.getElementById('dispaly').innerHTML = document.getElementById('my_form');
+    document.getElementById('display').innerHTML = document.getElementById('firstname');
+}
